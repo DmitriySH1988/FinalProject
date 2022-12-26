7d667b12eaf488b6de2f21daef66de6aba06548f
Dmitriy <Dimon8803@yandex.ru>
2022-12-26 22:23:07 +0300

    Added file README

--- FinalDz/Program.cs
+++ FinalDz/Program.cs
@@ -12,5 +12,19 @@ void SecArray(string[] array, string[] array2)
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
     }
 }
+void Printarray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecArray(array1,array2);
+Printarray(array2);
